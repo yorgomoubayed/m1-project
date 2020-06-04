338060d7feaa4e2372acd66fa4fa18241b48e203
Yorgo EL MOUBAYED <yorgo.el-moubayed@etu.univ-amu.fr>
2020-06-04 08:15:45 +0000

    Upload GSM3682164 dataset download bash script

---
+++ sra_explorer_fastq_download.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR876/005/SRR8768015/SRR8768015_1.fastq.gz -o SRR8768015_GSM3682164_GM18486_Rep1_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR876/005/SRR8768015/SRR8768015_2.fastq.gz -o SRR8768015_GSM3682164_GM18486_Rep1_Homo_sapiens_OTHER_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR876/006/SRR8768016/SRR8768016_1.fastq.gz -o SRR8768016_GSM3682164_GM18486_Rep1_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR876/006/SRR8768016/SRR8768016_2.fastq.gz -o SRR8768016_GSM3682164_GM18486_Rep1_Homo_sapiens_OTHER_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR876/004/SRR8768014/SRR8768014_1.fastq.gz -o SRR8768014_GSM3682164_GM18486_Rep1_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR876/004/SRR8768014/SRR8768014_2.fastq.gz -o SRR8768014_GSM3682164_GM18486_Rep1_Homo_sapiens_OTHER_2.fastq.gz
